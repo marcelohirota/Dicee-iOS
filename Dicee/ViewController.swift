4726ee70e94476e357e561ca5b5a6bd6b2a9e564
Marcelo Hirota <marcelohirota@gmail.com>
2023-05-17 16:35:52 +0930

    Adding functionality to button

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,18 +12,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     
     @IBOutlet weak var diceImageView2: UIImageView!
+
+    var i = 0
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFive")
+        diceImageView1.image = #imageLiteral(resourceName: "DiceOne")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceOne")
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped.")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        
+        i = Int.random(in: 0...4)
+        diceImageView1.image = diceArray[i]
+        i = Int.random(in: 0...4)
+        diceImageView2.image = diceArray[i]
     }
     
 }
