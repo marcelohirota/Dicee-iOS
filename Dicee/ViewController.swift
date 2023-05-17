f4d410cb67d80108ea1af5f9b1c5531a2025bd9b
Marcelo Hirota <marcelohirota@gmail.com>
2023-05-17 14:46:25 +0930

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Marcelo Hirota on 17/5/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
